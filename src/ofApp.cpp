915a62d83317f3fb996e95134ace502f7280cfa4
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2016-10-09 13:27:42 +0200

    Add laureates.xml loading and simple test

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,7 +1,47 @@
 #include "ofApp.h"
 
 void ofApp::setup(){
-
+	
+	// Load Nobel Price laureates here
+	bool loaded = xmlSettings.load("laureates.xml");
+	
+	if(!loaded){
+		cout << "Could not load laureates.xml" << endl;
+		ofExit();
+	}
+	
+	// Parse
+	if(xmlSettings.tagExists("laureates")){
+		xmlSettings.pushTag("laureates");
+		
+		unsigned int numLaureates = xmlSettings.getNumTags("laureate");
+		for(unsigned int i = 0; i < numLaureates; ++i){
+			xmlSettings.pushTag("laureate", i);
+			
+			Laureate * l = new Laureate();
+			l->first = xmlSettings.getValue("first", "Johny");
+			l->last = xmlSettings.getValue("last", "Mason");
+			l->field = xmlSettings.getValue("field", "Cooking");
+			l->year = xmlSettings.getValue("year", 1956);
+			laureates.push_back(l);
+			
+			xmlSettings.popTag(); // laureate
+		}
+		
+		xmlSettings.popTag(); // laureates
+	}else{
+		cout << "Wrong XML or it is empty" << endl;
+		ofExit();
+	}
+	
+	// Test print
+	for(unsigned int i = 0; i < laureates.size(); ++i){
+		cout << laureates[i]->first << ", "
+			<< laureates[i]->last << ", "
+			<< laureates[i]->field << ", "
+			<< laureates[i]->year
+			<< endl;
+	}
 }
 
 void ofApp::update(){
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,10 +1,22 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxXmlSettings.h"
 
 class ofApp : public ofBaseApp{
 	public:
 		void setup();
 		void update();
 		void draw();
+	
+		ofxXmlSettings xmlSettings;
+	
+		struct Laureate {
+			string first;
+			string last;
+			string field;
+			int year;
+		};
+	
+		vector <Laureate *> laureates;
 };
