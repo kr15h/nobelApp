9f8864e74b3c76481e4f7da0f62d0dbeff046eb4
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2016-10-10 18:12:16 +0200

    Add APP_MARGIN and redefine other constants

--- src/Constants.h
+++ src/Constants.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#define APP_WIDTH 1700
+#define APP_HEIGHT 300
+
+#define BIG_FONT_SIZE 75
+#define SMALL_FONT_SIZE 25
+
+#define APP_MARGIN 40
+
 #define XML_SETTINGS_FILENAME "laureates.xml"
 
 #define REGULAR_FONT_PATH "Libre_Baskerville/LibreBaskerville-Regular.ttf"
 #define BOLD_FONT_PATH "Libre_Baskerville/LibreBaskerville-Bold.ttf"
-
-#define BIG_FONT_SIZE 50
-#define SMALL_FONT_SIZE 25
